0060d04418580fc203df0d4304ab33576fe251a9
Mac Ryan <quasipedia@gmail.com>
2011-08-30 20:41:49 +0200

    Intial commit of sphinx files

---
+++ docs/doc.sh
@@ -0,0 +1,29 @@
+# /bin/bash
+# builds documentation and pushes it to GitHub Pages
+
+# only committed changes must be documented!
+git stash
+
+# make a clean build of the html files and moves it out of the way
+rm -Rf _build/
+mkdir _build
+make html
+mkdir /tmp/atc-docs
+cp -R _build/html/* /tmp/atc-docs/
+rm -Rf _build/
+
+# switch to GitHub pages repository and imports the build
+cd ..
+git checkout gh-pages
+rm -Rdf *
+cp -R /tmp/atc-docs/* .
+rm -Rf /tmp/atc-docs
+
+# track changes and pushes them to the server
+git add *.html *.js objects.inv _static/* _sources/*
+git commit -m "automatic documentation sync"
+#git push
+
+# revert to normal editing mode
+git checkout master
+git stash pop
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Air Traffic Controller - Next Generation documentation master file, created by
+   sphinx-quickstart on Tue Aug 30 20:38:35 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Air Traffic Controller - Next Generation's documentation!
+====================================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
