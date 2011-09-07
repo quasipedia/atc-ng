07819dad5d940b779e3b8d8a45fb285e3b78891b
Mac Ryan <quasipedia@gmail.com>
2011-09-07 08:00:48 +0200

    doc.sh script with basic error handling

--- docs/doc.sh
+++ docs/doc.sh
@@ -1,20 +1,32 @@
 # /bin/bash
 # builds documentation and pushes it to GitHub Pages
 
+# The clean command might throw errors (trying to delete non-existent files), so
+# we want to prevent it to stop the script from working.
+function make_clean {
+    set +e
+    make clean
+    set -e
+}
+
+# exit immediately in case of errors
+set -e
+
 # only committed changes must be documented!
 git stash
 
-# make a clean build of the html files and moves it out of the way
-make clean
+# make a clean build of the html files and moves it out of the way.
+make_clean
 make html
 mkdir /tmp/atc-docs
 cp -R _build/html/* /tmp/atc-docs/
-make clean
+make_clean
 
 # save aptana project files
 cd ..
 mkdir /tmp/atc-project-files
-cp .project .pydevproject /tmp/atc-project-files
+mv .project .pydevproject /tmp/atc-project-files
+
 # switch to GitHub pages repository and imports the build
 git checkout gh-pages
 rm -rdf *
